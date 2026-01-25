45787da31513c3a6756e2266bcec41383b732482
lmaooo-tech <ocoijosephat@gmail.com>
2026-01-25 23:51:09 +0300

    >>

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,15 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
-class CustomUserManager(UserManager):
+class CustomUserManager(BaseUserManager):
 	"""Custom manager to handle user creation with extra fields."""
+
+	use_in_migrations = True
+
+	def _create_user(self, username, email, password, **extra_fields):
+		if not username:
+			raise ValueError('The username must be set')
+		email = self.normalize_email(email)
+		user = self.model(username=username, email=email, **extra_fields)
+		user.set_password(password)
+		user.save(using=self._db)
+		return user
 
 	def create_user(self, username, email=None, password=None, **extra_fields):
 		extra_fields.setdefault('is_staff', False)
 		extra_fields.setdefault('is_superuser', False)
-		user = super().create_user(username=username, email=email, password=password, **extra_fields)
-		return user
+		return self._create_user(username, email, password, **extra_fields)
 
 	def create_superuser(self, username, email=None, password=None, **extra_fields):
 		extra_fields.setdefault('is_staff', True)
@@ -20,8 +30,7 @@ class CustomUserManager(UserManager):
 		if extra_fields.get('is_superuser') is not True:
 			raise ValueError('Superuser must have is_superuser=True.')
 
-		user = super().create_superuser(username=username, email=email, password=password, **extra_fields)
-		return user
+		return self._create_user(username, email, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
