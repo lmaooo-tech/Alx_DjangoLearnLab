b701d12b3d16386abf2abc095e9ac3559332fd58
lmaooo-tech <ocoijosephat@gmail.com>
2026-01-26 00:16:48 +0300

    >>>

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -4,3 +4,8 @@ from django import forms
 class BookSearchForm(forms.Form):
     """Simple form to validate book title searches safely."""
     query = forms.CharField(max_length=200, required=False, strip=True)
+
+
+class ExampleForm(forms.Form):
+    """Example form demonstrating CSRF-protected input handling."""
+    sample = forms.CharField(max_length=100, required=True, strip=True)
