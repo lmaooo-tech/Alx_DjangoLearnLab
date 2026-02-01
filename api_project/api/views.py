76f3e2a767fbd9c1caa740b9642424c04aa211ff
lmaooo-tech <ocoijosephat@gmail.com>
2026-02-01 17:15:53 +0300

    >>>

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,6 +4,6 @@ from .models import Book
 from .serializers import BookSerializer
 
 
-class BookList(generics.ListCreateAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
