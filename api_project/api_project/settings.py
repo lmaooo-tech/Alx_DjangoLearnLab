fdcba3b1e9c2e908e21177a6b7cd4bed12e29813
lmaooo-tech <ocoijosephat@gmail.com>
2026-02-01 18:03:20 +0300

    >>>

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    'rest_framework.authtoken',
     'api',
     'rest_framework.authtoken',
+    'rest_framework.permissions.IsAuthenticated',
 ]
 
 MIDDLEWARE = [
