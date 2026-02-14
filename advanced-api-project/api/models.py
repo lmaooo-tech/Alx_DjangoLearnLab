d6a37cce526ea7d0bed05aecb27ad06c89c41940
lmaooo-tech <ocoijosephat@gmail.com>
2026-02-14 12:02:06 +0300

    Created Custom serializers

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    """
+    Author model to store author information.
+    """
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    """
+    Book model to store book information.
+    Establishes a one-to-many relationship with Author.
+    """
+    title = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return self.title
